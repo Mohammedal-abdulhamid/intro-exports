36606be4188dc01fa79a80e2cde2478ddc1d1c85
Liam310 <liam.duncan310@gmail.com>
2023-03-21 15:40:53 +0000

    amend example

--- functions/section-1/challenges.js
+++ functions/section-1/challenges.js
@@ -18,8 +18,8 @@ This function should return the product of two passed numbers
 */
 
 runTest("multiply() can multiply 2 numbers together", function () {
-  check(multiply(10, 42)).isEqualTo(420);
   check(multiply(3, 5)).isEqualTo(15);
+  check(multiply(17, 19)).isEqualTo(323);
   check(multiply(-180, 2)).isEqualTo(-360);
 });
 
